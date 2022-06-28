140686fc5a19440efb79ae45a61c77ef2bb7faa8
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2022-06-28 14:25:47 +0700

    Boilerplate pokemon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import PokemonList from './Pages/PokemonList';
+import PokemonDetail from './Pages/PokemonDetail';
+import MyPokemon from './Pages/MyPokemon';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<PokemonList/>} />
+          <Route path='/pokemon-detail' element={<PokemonDetail/>} />
+          <Route path='/my-pokemon' element={<MyPokemon/>} />
+        </Routes>
+      </Router>
   );
 }
 
---
+++ src/Pages/PokemonList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PokemonList = () => {
+  return (
+    <div className='font-semibold text-slate-500'>
+      PokemonList
+    </div>
+  )
+}
+
+export default PokemonList
